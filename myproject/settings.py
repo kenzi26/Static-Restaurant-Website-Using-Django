27b34fb421055d6bbe1e11543eb9fcdebfafe4c5
kenzi26 <mokwenyekene1@yahoo.com>
2023-10-23 19:23:18 +0100

    Feat: modification to project section

--- myproject/settings.py
+++ myproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3=9_a5*jpwro+(h-$&=z32lxr5cstbuqp3r*@&_eyxmpwf53x^'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
